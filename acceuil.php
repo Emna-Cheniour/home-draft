553ad8f65d668d629b12f71ec41e4a3cdc9d6671
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-04-11 00:49:26 +0100

    added services

--- acceuil.php
+++ acceuil.php
@@ -1,27 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min (1).css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/Titles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
 
     <title>Celiac101</title>
 </head>
+
 <body>
     <?php include("navbar.php"); ?>
     <div class="services">
         <h1>Services</h1>
-        <div class="services wrapper">
-            <div class="service card"></div>
+        <div class="serviceRow row">
+            <div class="service col4">
+                <img src="images/panier.png">
+                <h2>Vente des produits</h2>
+                <p>Nous offrons un catalogue contenant une grande varieté des produits sans gluten.
+                    <br>
+                    Visitez notre catalogue et jouissez de la qualité au meilleur prix.
+                </p>
+                <div class="services__btn"><button class="btn btn1">Catalogue</button></div>
+            </div>
+            <div class="service col4">
+                <img src="images/recette.png">
+                <h2>Recettes</h2>
+                <p>Cuisiner sans gluten est l'une des tâches les plus délicates en cuisine.
+                    <br>
+                    Pour ceci, Celiac101 vous offre des recettes testées et leurs démarches simplifiées.
+                </p>
+                <div class="services__btn"><button class="btn btn2">Recettes</button></div>
+            </div>
+            <div class="service col4">
+                <img src="images/aliment.png">
+                <h2>Journal Alimentaire</h2>
+                <p>Toujours incertain entre aliments interdits et aliments sans gluten?
+                    <br>
+                    Notre journal des aliments omettera toute incertitude !
+                </p>
+                <div class="services__btn"><button class="btn btn1">Consulter Journal</button></div>
+            </div>
+            <div class="service col4">
+                <img src="images/resto.png">
+                <h2>Carte</h2>
+                <p>Celiac101 offre une carte speciale coeliaques !
+                    <br>
+                    Decouvrez les localisations des restaurants en Tunisie avec des repas sans gluten et plus.
+                </p>
+                <div class="services__btn"><button class="btn btn2">Liste Des Restaurants</button></div>
+            </div>
+            <div class="service col4">
+                <img src="images/faq.png">
+                <h2>FAQs</h2>
+                <p>Des réponses aux questions les plus posées par et aux les coeliaques.
+                    <br>
+                    Découvrez les maintenant!
+                </p>
+                <div class="services__btn"><button class="btn btn1">FAQ</button></div>
+            </div>
+            <div class="service col4">
+                <img src="images/nutritionist.png">
+                <h2>Spécialistes</h2>
+                <p>Plusieurs medecins et specialistes sont là pour vous !
+                    <br>
+                    Vous pouvez les contacter et demander leurs conseils.
+                </p>
+                <div class="services__btn"><button class="btn btn2">Contact</button></div>
+            </div>
         </div>
     </div>
-    
-    
 
-<script src="js/main.js" ></script>
+
+
+    <script src="js/main.js"></script>
 
 </body>
+
 </html>
