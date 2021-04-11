1ef963bf465e85c676b06d56c85d7934ef178d6f
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-04-11 01:27:35 +0100

    fixed bug concerning dropdiv in phone

--- Js/main.js
+++ Js/main.js
@@ -2,12 +2,12 @@ const drop= document.querySelector('#drop');
 const dropPhone= document.querySelector('.dropPhone');
 const dropdiv = document.querySelector(".dropdown");
 const dropdivPhone = document.querySelector(".dropdownForPhone");
-function show(x,y){ //Fonction pour afficher ou enlever un element par clic sur un autre
+function show(x,y,d){ //Fonction pour afficher ou enlever un element par clic sur un autre
     x.addEventListener("click",()=>{
         var display= y.style.display;
-        if((display=="none")) y.style.display="block";
+        if((display=="none")) y.style.display=d;
          else y.style.display="none";
      })
 }
-show(drop,dropdiv);
-show(dropPhone,dropdivPhone);
+show(drop,dropdiv,"block");
+show(dropPhone,dropdivPhone,"inline-block");
