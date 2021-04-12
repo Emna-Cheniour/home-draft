e2e67833647122e7b9e2fa6b5859a468795cf852
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-04-12 23:22:39 +0000

    fixed bugs with navbar

--- navbarDeconnecte.php
+++ navbarDeconnecte.php
@@ -1,5 +1,5 @@
 <div class="navbar-custom">
-    <img class="logopic" src="images/logonav1.png" alt="Logo">
+    <img class="logopic" src="images/logo1.png" alt="Logo">
     <span class="pagename">Celiac101</span>
     <ul class='navLinks'>
         <li><a href="acceuil.php">Acceuil </a></li>
