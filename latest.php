2de14ee71e7613278c4e76dfb710dc5be2edf9df
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-04-28 16:21:28 +0000

    shopping

---
+++ latest.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'assets/mainHead.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include_once 'navbarConnecte.php' ?>
+    <div class="mainLatest">
+        
+    <h2>Latest Products</h2>
+        <div class="cardLatest">
+            <div class="circle">
+            </div>
+            <div class="content">
+                <h2>Pepsi cola</h2>
+                <p>Refrshing SOda from the UK </p>
+            </div>
+            <img src="images/pepsi.png" alt="">
+        </div>
+        <div class="cardLatest">
+            <div class="circle">
+            </div>
+            <div class="content">
+                <h2>Pepsi cola</h2>
+                <p>Refrshing SOda from the UK </p>
+            </div>
+            <img src="images/pepsi.png" alt="">
+        </div>
+               <div class="cardLatest">
+            <div class="circle">
+            </div>
+            <div class="content">
+                <h2>Pepsi cola</h2>
+                <p>Refrshing SOda from the UK </p>
+            </div>
+            <img src="images/pepsi.png" alt="">
+        </div>
+    </div>
+</body>
+
+</html>
