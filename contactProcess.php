01d4d75353cb43428935369ccb8b9582bed7f8ee
meriem ben ismail <meriembenismail@yahoo.com>
2021-05-21 00:29:28 +0100

    profile edit

--- contactProcess.php
+++ contactProcess.php
@@ -5,19 +5,26 @@ include_once ("autoload.php");
 
 $users = new UserRepository();
 
+if ( empty($_POST['adresse'])  &&  empty($_POST['telephone']) ) {
 
-
-if (isset($_POST['adresse']) && !(empty($_POST['adresse'])) && isset($_POST['telephone']) && !(empty($_POST['telephone']))) {
-  $adresse=$_POST['adresse'];
-  $users->update('adresse', $adresse);
-  
-  $telephone=$_POST['telephone'];
-  $users->update('telephone', $telephone);
-}
-else {
-  $_SESSION['contactError']='Veuillez vérifier les informations saisies';
+  $_SESSION['contactError']='Veuillez vérifier les informations saisies' ;
   $_SESSION['page']='contact';
   $_SESSION['pageIcon']='active';
+
+
+
+} else {
+  
+    if(isset($_POST['adresse']) && !(empty($_POST['adresse']))){
+      $adresse=$_POST['adresse'];
+      $users->update('adresse', $adresse);
+    }
+
+    if(isset($_POST['telephone']) && !(empty($_POST['telephone']))){
+      $telephone=$_POST['telephone'];
+      $users->update('telephone', $telephone);
+    }
 }
+
 
 header('location:profile.php');
