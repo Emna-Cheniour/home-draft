37ccda8b062b19d5d6361d8eb660f969cc3b7e95
Emna-Cheniour <78973493+Emna-Cheniour@users.noreply.github.com>
2021-05-22 12:00:12 +0100

    journal

---
+++ journalIndiv.php
@@ -0,0 +1,78 @@
+<?php include_once 'head.php' ?>
+<body>
+<?php include_once 'navbarConnecte.php'?>
+
+<body>
+<div class="journalIndivContainer">
+<div class="searchJournal__input">
+        <i class="fas fa-search" id="search__icon"></i>
+        <input id="search" placeholder="Rechercher..." type="text">
+
+      </div>
+        <div class="rowJournalIndiv">
+        <div class="allowed">
+            <div class="journalTitre">
+            <h4>
+                A privilégier
+                <img src="icons/winking.png">
+            </h4>
+           
+        </div>
+        <ul>
+            <li>
+                product1
+            </li>
+            <li>
+                product2
+            </li>
+            <li>
+                product3
+
+            </li>
+        </ul>
+        </div>
+        <div class="modere">
+        <div class="journalTitre">
+            <h4>
+                A tolérer
+                <img src="icons/neutral.png">
+            </h4>
+        </div>
+        <ul>
+            <li>
+                product1
+            </li>
+            <li>
+                product2
+            </li>
+            <li>
+                product3
+
+            </li>
+        </ul>
+        </div>
+        <div class="forbidden">
+        <div class="journalTitre">
+            <h4>
+                A interdire
+                <img src="icons/angry.png">
+            </h4>
+        </div>
+        <ul>
+            <li>
+                product1
+            </li>
+            <li>
+                product2
+            </li>
+            <li>
+                product3
+
+            </li>
+        </ul>
+        </div>
+    </div>
+    </div>
+    <?php include_once 'footer.php'?>
+</body>
+</html>
