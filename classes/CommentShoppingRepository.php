7bd7a645c3a4ccd355d2c709b147ea7b31f1fc26
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-06-13 00:28:50 +0100

    buying

---
+++ classes/CommentShoppingRepository.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'autoload.php';
+
+class CommentShoppingRepository extends Repository
+{
+    public function __construct()
+    {
+        parent::__construct('commentshopping');
+    }
+ 
+     
+
+}
+?>
--- test.php
+++ test.php
@@ -1,8 +1,6 @@
 <?php
 require_once 'autoload.php';
 
-$rp =new ProductRepository();
-$result= $rp->update(array('where'=>array("id"=>28),'value'=>array('price'=>35,'promotion'=>5)));
-
-
-var_dump($result);
+$cartRep = new CartRepository();
+$result=$cartRep->insert(array('status'=>'en cours'));
+echo $result;
