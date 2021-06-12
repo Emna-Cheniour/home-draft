f7f1b97f53ba717f20e908ea07b723341f70d4a9
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-06-12 09:42:23 +0100

    sql

--- classes/SpecialistRepository.php
+++ classes/SpecialistRepository.php
@@ -9,6 +9,7 @@ class SpecialistRepository extends Repository
     }
     public function insert($criterias){
        $req=(array('id' => "", 'firstName' => $criterias[0],'lastName'=> $criterias[1], 'email'=>  $criterias[2] , 'address'=> $criterias[3], 'phoneNumber'=> $criterias[4], 'profession'=> $criterias[5] ,'description'=> $criterias[6]));
+       
         parent::insert($req);
 
     }
