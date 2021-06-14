bcbb69520cb05e0def54f1a7a4373556aa0aec1c
nawresncib12 <nawresncib@insat.u-carthage.tn>
2021-06-14 11:52:54 +0100

    video acceuil

--- acceuil.php
+++ acceuil.php
@@ -1,28 +1,30 @@
-<?php 
+<?php
 
 session_start();
 
-$pageName='Acceuil';
+$pageName = 'Acceuil';
 include("assets/mainHead.php");
 include_once 'autoload.php';
 
 ?>
 
 </head>
+
 <body>
-    <?php 
- 
-    //include_once 'preloader.php' ?>
     <?php
-if (isset($_SESSION['user'])) {
+
+    //include_once 'preloader.php' 
+    ?>
+    <?php
+    if (isset($_SESSION['user'])) {
 
         include_once "navbarCo.php";
         include_once 'profileProgress.php';
         $infoCount *= 10;
         if ($infoCount != 100) {
-          
-           ?>
-           <ul class="welcome">
+
+    ?>
+            <ul class="welcome">
                 <li>B</li>
                 <li>I</li>
                 <li>E</li>
@@ -31,9 +33,9 @@ if (isset($_SESSION['user'])) {
                 <li>E</li>
                 <li>N</li>
                 <li>U</li>
-                
+
             </ul>
-           <?php
+    <?php
         }
     } else {
         include "navbarDeco.php";
@@ -42,10 +44,11 @@ if (isset($_SESSION['user'])) {
 
     ?>
 
+
     <div class="poster">
         <div class="posterDetails">
             <div class="c4 sign">
-                <h2>Qui sommes nous?</h2>
+                <h2>CELIAC101</h2>
             </div>
             <p> Votre meilleur guide pour vivre avec la maladie coeliaque </p>
             <p>Celiac 101 vous offre un ensemble de services qui facilitent votre quotidien et vous aident à vous adapter au régime alimentaire sans gluten
@@ -53,6 +56,19 @@ if (isset($_SESSION['user'])) {
             <p>Bienvenu au premier site web d'origine tunisienne pour les malades coeliaques. </p>
         </div>
         <div class="pimg"><img class="posterImg" src="images/poster picture.png" alt=""></div>
+    </div>
+    <div class="posterV">
+
+        <video class="posterVid" playsinline autoplay muted loop>
+            <source src="images/poster.mp4" type="video/mp4">
+        </video>
+        
+        <div class="posterDetails">
+            <p> La maladie coeliaque est plus récurrente que vous pensez </p>
+            <p>Conçu par une équipe comportant des personnes eux même atteints ce guide répond à tout besoin d'un coeliaque
+            </p>
+            <p>Naviguez Celiac101 pour découvrir ses multiples services</p>
+        </div>
     </div>
 
     <div class="services">
@@ -65,7 +81,7 @@ if (isset($_SESSION['user'])) {
                     <br>
                     Visitez notre catalogue et jouissez de la qualité à meilleur prix.
                 </p>
-                <div class="services__btn" ><a href="shopping.php" ><button class="btn btn1 btnServ">Catalogue</button></a></div>
+                <div class="services__btn"><a href="shopping.php"><button class="btn btn1 btnServ">Catalogue</button></a></div>
             </div>
             <div class="service col4">
                 <img src="images/recette.png">
@@ -74,7 +90,7 @@ if (isset($_SESSION['user'])) {
                     <br>
                     C'est pourquoi Celiac101 vous offre des recettes testées avec leurs démarches simplifiées.
                 </p>
-                <div class="services__btn"><a href="recette.php" ><button class="btn btn2 btnServ">Recettes</button></a></div>
+                <div class="services__btn"><a href="recette.php"><button class="btn btn2 btnServ">Recettes</button></a></div>
             </div>
             <div class="service col4">
                 <img src="images/aliment.png">
@@ -83,7 +99,7 @@ if (isset($_SESSION['user'])) {
                     <br>
                     Notre journal des aliments omettera toute incertitude !
                 </p>
-                <div class="services__btn"><a href="Journal.php"><button class="btn btn1 btnServ" >Journal</button></a></div>
+                <div class="services__btn"><a href="Journal.php"><button class="btn btn1 btnServ">Journal</button></a></div>
             </div>
             <div class="service col4">
                 <img src="images/resto.png">
@@ -151,71 +167,71 @@ if (isset($_SESSION['user'])) {
             </div>
 
             <div class="row rowy quizz reveal">
-                
-           
-               <div class="col9">
-                <?php if(isset($_SESSION['user'])){
-                    $username=$_SESSION['user'];
-                    if($infoCount!=100){
-                     echo "<h2 class='c4 faded' >Bienvenu $username</h2>";
-                     echo "<p>Votre Profil est à $infoCount %</p>";
-                     echo "<a href='profile.php'><button class='btn btn3'>Complétez votre profil<i class='fas fa-arrow-alt-circle-right'></i></button></a>";
-                    }
-                } ?>
-               
 
-               
 
-               
-           </div> 
-
-        </div>
+                <div class="col9">
+                    <?php if (isset($_SESSION['user'])) {
+                        $username = $_SESSION['user'];
+                        if ($infoCount != 100) {
+                            echo "<h2 class='c4 faded' >Bienvenu $username</h2>";
+                            echo "<p>Votre Profil est à $infoCount %</p>";
+                            echo "<a href='profile.php'><button class='btn btn3'>Complétez votre profil<i class='fas fa-arrow-alt-circle-right'></i></button></a>";
+                        }
+                    } ?>
 
 
 
 
-    </div>
 
-    <div class="row">
-
-
-        <div class="col specialist__section">
-
-            <div class="suggestionsSpecialist">
-
-                <h4 class="specialistsText">Vous avez besoin d'un
-                    <span class="txt-rotate" data-period="2000" data-rotate='["medécin?","nutritioniste?","gastrologue?"]'></span>
-                </h4>
-
+                </div>
 
             </div>
 
+
+
+
         </div>
 
+        <div class="row">
 
-    </div>
+
+            <div class="col specialist__section">
+
+                <div class="suggestionsSpecialist">
+
+                    <h4 class="specialistsText">Vous avez besoin d'un
+                        <span class="txt-rotate" data-period="2000" data-rotate='["medécin?","nutritioniste?","gastrologue?"]'></span>
+                    </h4>
+
+
+                </div>
+
+            </div>
+
+
+        </div>
 
     </div>
     <div class="specialistsAd">
         <div class="swiper-container specialistsContainer">
             <div class="swiper-wrapper specialistsWrapper">
-                <?php 
-                    $specialistRepo=new SpecialistRepository();
-                    $specialists=$specialistRepo->findAll();
+                <?php
+                $specialistRepo = new SpecialistRepository();
+                $specialists = $specialistRepo->findAll();
 
-                    foreach($specialists as $specialist){
+                foreach ($specialists as $specialist) {
                 ?>
-                <div class="swiper-slide specialistSlide">
-                    <div class="specialistAd">
-                        <img src=<?php echo "data:image/jpeg;base64," . base64_encode($specialist['image']) ?> alt="">
-                        <h4><?php echo $specialist['firstName']." ".$specialist['lastName']?></h4>
-                        <h5><?php echo $specialist['profession']?></h5>
+                    <div class="swiper-slide specialistSlide">
+                        <div class="specialistAd">
+                            <img src=<?php echo "data:image/jpeg;base64," . base64_encode($specialist['image']) ?> alt="">
+                            <h4><?php echo $specialist['firstName'] . " " . $specialist['lastName'] ?></h4>
+                            <h5><?php echo $specialist['profession'] ?></h5>
+                        </div>
                     </div>
-                </div>
                 <?php } ?>
-               
-               
-          
+
+
+
             </div>
         </div>
     </div>
@@ -223,18 +239,18 @@ if (isset($_SESSION['user'])) {
 
 
 
-  
+
     <?php include 'footer.php' ?>
-<?php include_once 'assets/scripts.php' ?>
+    <?php include_once 'assets/scripts.php' ?>
     <script src="js/main.js"></script>
 
     <script src="js/txtRotation.js"></script>
     <script type="js/scrollUpBtn.js"></script>
     <script src="js/testnav.js"></script>
     <script src="js/specialistIndiv.js"></script>
-    
-    
-   
+
+
+
 
 </body>
 
--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'autoload.php';
-
-$cmdRep=new CommandRepository();
-$cmdRep->addCommand(array('nawres04',date('Y-m-d'),'submitted',14,'sgg',3354,date('Y-m-d'),'sgwfg'));
